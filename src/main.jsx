0eca8a40c23e05af338c48d10dd8fc0b35f2020c
Avinash sharma <sharmaavi2196@gmail.com>
2025-01-21 22:06:49 +0530

    star rating component with messages instead of numbers displayed using messages array in the props

--- src/main.jsx
+++ src/main.jsx
@@ -7,5 +7,11 @@ import StarRating from "./StarRating";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      color="red"
+      messages={["Terrible", "bad", "okay", "good", "Amazing"]}
+      size={24}
+    />
   </StrictMode>
 );
